b6861a0c908e16c1eabd17809abb87332f296e5f
Luis Guzman <luis.dev@lmaero.pro>
2023-03-12 12:18:26 -0500

    Add navigation between fragments

--- app/src/main/java/com/example/alarmas/Overview.kt
+++ app/src/main/java/com/example/alarmas/Overview.kt
@@ -1,15 +1,11 @@
 package com.example.alarmas
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +13,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Overview : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Overview.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Overview().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val micButton: Button = view.findViewById(R.id.continueButton)
+        micButton.setOnClickListener {
+            val fragment = AlarmsList()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.nav_host_fragment, fragment)?.commit()
+        }
+        return view
     }
 }
